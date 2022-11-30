41d86eadb290c7a94a31f27df705a6f564aea92d
Yasuhiro-Ikeda-57 <81315087+Yasuhiro-Ikeda-57@users.noreply.github.com>
2022-11-30 21:39:41 +0900

    third commit

---
+++ dash.py
@@ -0,0 +1,18 @@
+﻿import pandas as pd
+df = pd.read_excel('fat.xlsx')
+
+# https://qiita.com/inoory/items/12028af62018bf367722
+
+import plotly.graph_objects as go
+fig = go.Figure(data=[
+    go.Scatter(x=df['date'],
+               y=df['weight'],
+               name='体重'
+              ),
+    go.Scatter(
+               x=df['date'],
+               y=df['fat'],
+               name='体脂肪率'
+              ),
+])
+fig.show()
