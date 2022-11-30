84cb9f9a733bb661cc8b23d3eeca0c937d18a64f
Yasuhiro-Ikeda-57 <81315087+Yasuhiro-Ikeda-57@users.noreply.github.com>
2022-11-30 22:53:37 +0900

    third commit-5

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-import pandas as pd
-import plotly.graph_objects as go
 
 @app.route('/')
 def hello_world():
-    df = pd.read_excel('fat.xlsx')
-
-    fig = go.Figure(data=[
-        go.Scatter(x=df['date'],
-               y=df['weight'],
-               name='体重'
-              ),
-        go.Scatter(
-               x=df['date'],
-               y=df['fat'],
-               name='体脂肪率'
-              ),
-    ])
-
-    return fig.show()
+   return 'ただのHello World'
