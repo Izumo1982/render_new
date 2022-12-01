dbcf0e9262031c7f77c9166a400b8158bc98efdc
Yasuhiro-Ikeda-57 <81315087+Yasuhiro-Ikeda-57@users.noreply.github.com>
2022-12-01 22:49:20 +0900

    'time'

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'ただのHello World'
+   now = datetime.datetime.now() 
+   return now.strftime('今日は%Y年%m月%d日\n今は%H時%M分%S秒') + '\n\nただのHello World'
